48a2a36d8dcc68664995e5ea959ab1208f538c83
Joki <josedavidamayar@gmail.com>
2024-03-11 22:26:23 -0500

    Solution of point 12

--- src/co/uniquindio/laboratorioListas/laboratorio/Exercises.java
+++ src/co/uniquindio/laboratorioListas/laboratorio/Exercises.java
@@ -6,6 +6,9 @@ import co.uniquindio.laboratorioListas.listas.DoubleLinkedList;
 import co.uniquindio.laboratorioListas.listas.LinkedList;
 import co.uniquindio.laboratorioListas.model.People;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -84,7 +87,7 @@ public class Exercises {
         System.out.println(" ");
         System.out.println("2. Obtain the list of people who have an ID with an even number of numbers from a simple linked list of people.");
         ArrayList<People> result2 = getPeopleWithEvenID(peopleList);
-        System.out.println("People with even identification: "+result2);
+        System.out.println("People with even identification: " + result2);
 
         //3. Remove even numbers from a simple linked list
         System.out.println(" ");
@@ -114,7 +117,7 @@ public class Exercises {
         System.out.println("5. Write a method that returns the number of times a value is repeated in a linked list.");
         int var = 3;
         int result5 = returnAppearances(var, numberList);
-        System.out.println("The appearances of the value "+var+" are: "+result5);
+        System.out.println("The appearances of the value " + var + " are: " + result5);
 
         //6. Write the printBackward() method of a doubly linked list.
         System.out.println(" ");
@@ -154,6 +157,12 @@ public class Exercises {
         System.out.println("The final number list is: ");
         numberList.printList();
 
+        //12. Write a program that calculates the mean and standard deviation of a set of N real numbers
+        System.out.println("Solution 12:");
+        LinkedList<Double> linkedList = Exercises.createLinkedListFromFile("src/resources/values.txt");
+        linkedList.printList();
+        System.out.println("The mean is: " + getMean());
+        System.out.println("The standard deviation is: " + getStandartDeviation());
     }
 
     //1. Get the numbers in the odd positions of a simple linked list of numbers
@@ -164,9 +173,9 @@ public class Exercises {
         int i = 0;
         //Instance of iterator for traveling the list
         Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             //If the index is odd, the value of the node is added to the list
-            if(i%2==1){
+            if (i % 2 == 1) {
                 posOdds.add(list.getNodeValue(i));
             }
             iterator.next();
@@ -176,16 +185,16 @@ public class Exercises {
     }
 
     //2. Obtain the list of people who have an ID with an even number of numbers from a simple linked list of people.
-    public static ArrayList<People> getPeopleWithEvenID(LinkedList<People> list){
+    public static ArrayList<People> getPeopleWithEvenID(LinkedList<People> list) {
         //Initialize an ArrayList that contains the People that have even ID
         ArrayList<People> peopleList = new ArrayList<People>();
         //Instance of iterator for traveling the list
         Iterator<People> iterator = list.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             //Get the Persona in the Node
             People p = iterator.next();
             //If amount of digits of the Person is a even number, it´s added to the popleList
-            if(p.isIdentificationEven()){
+            if (p.isIdentificationEven()) {
                 peopleList.add(p);
             }
         }
@@ -193,47 +202,47 @@ public class Exercises {
     }
 
     //3. Remove even numbers from a simple linked list
-    public static void removeEvenNumbers(LinkedList<Integer> list){
+    public static void removeEvenNumbers(LinkedList<Integer> list) {
         //Index for position reference
         //Instance of iterator for traveling the list
         Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             //Get the Node value
             int v = iterator.next();
             //If the Node value is a even number, it´s removed form the list
-            if(v%2==0){
+            if (v % 2 == 0) {
                 list.delete(v);
             }
         }
     }
 
     //4. Write a method that returns a linked list with the odd values of a list of numbers.
-    public static LinkedList<Integer> returnOddValues(LinkedList<Integer> numberList){
+    public static LinkedList<Integer> returnOddValues(LinkedList<Integer> numberList) {
         //Create a instance of a linked list, where the odd values are deposited
         LinkedList<Integer> oddList = new LinkedList<>();
         //Index for oddlist position reference
         int j = 0;
         //Instance of iterator for traveling the list
         Iterator<Integer> iterator = numberList.iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             //Get the value from de node on i position
             int v = iterator.next();
             //If the Node value is an odd number, it´s added to the linked list
-            if(v%2!=0){
-               oddList.add(v, j);
-               j++;
+            if (v % 2 != 0) {
+                oddList.add(v, j);
+                j++;
             }
         }
         return oddList;
     }
 
     //5. Write a method that returns the number of times a value is repeated in a linked list.
-    public static <T extends Comparable<T>> int returnAppearances(T value, LinkedList<T> list){
+    public static <T extends Comparable<T>> int returnAppearances(T value, LinkedList<T> list) {
         return list.countAppearances(value);
     }
 
     //6. Write the printBackward() method of a doubly linked list.
-    public static <T extends Comparable<T>> void printBackward(DoubleLinkedList<T> list){
+    public static <T extends Comparable<T>> void printBackward(DoubleLinkedList<T> list) {
         System.out.println("The double linked list printed in backwards is:");
         list.printBackward();
     }
@@ -241,23 +250,23 @@ public class Exercises {
     //7. Write the Iterator for a doubly linked list.
     //The Iterator is writen in the DoubleLinkedList class
     //This is a test for the iterator
-    public static <T extends Comparable<T>> void testDoublyListIterator(DoubleLinkedList<T> list){
+    public static <T extends Comparable<T>> void testDoublyListIterator(DoubleLinkedList<T> list) {
         for (T t : list) {
             System.out.print("Elemento: " + t + " ");
         }
     }
 
     //8. Obtain the list of people who have an ID with even number of elements from a list double linked of people.
-    public static ArrayList<People> getPeopleEvenID(DoubleLinkedList<People> list){
+    public static ArrayList<People> getPeopleEvenID(DoubleLinkedList<People> list) {
         //Initialize a instance of a list, that will contain the people with even number of digits in their ID
         ArrayList<People> evenID = new ArrayList<People>();
         //Instance of the list iterator for traveling the list
         Iterator<People> iterator = list.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             //Get the value of the node
             People p = iterator.next();
             //Verification - if the person has even ID is added to the final list
-            if(p.isIdentificationEven()){
+            if (p.isIdentificationEven()) {
                 evenID.add(p);
             }
         }
@@ -268,7 +277,7 @@ public class Exercises {
     // Method for insert on CircularLinkedList on class CircularLinkedList
     // Method for search on CircularLinkedList on class CircularLinkedList
     // Method for testing
-    public static void circularLinkedListMethodsTest(){
+    public static void circularLinkedListMethodsTest() {
         CircularLinkedList<Integer> list = new CircularLinkedList<>();
 
         // Add elements to the circular linked list
@@ -284,13 +293,68 @@ public class Exercises {
 
         //Test for search method
         int p = list.search(4);
-        System.out.println("The position of the value: "+"4"+" is: "+p);
+        System.out.println("The position of the value: " + "4" + " is: " + p);
     }
 
     //10. Write a method that allows “concatenating” two simple linked lists, the method must
     //receive as parameters two objects of type List, join them and return a list that contains
     //to both.
-    public static <T extends Comparable<T>> void concatLinkedList(LinkedList<T> list1, LinkedList<T> list2){
+    public static <T extends Comparable<T>> void concatLinkedList(LinkedList<T> list1, LinkedList<T> list2) {
         list1.concat(list2);
+    }
+
+    //12. Write a program that calculates the mean and standard deviation of a set of N real numbers
+    /**
+     * Gets the mean of n values
+     * @return
+     */
+    public static double getMean() {
+        LinkedList<Double> linkedList = createLinkedListFromFile("src/resources/values.txt");
+        double sum = 0;
+        int count = 0;
+        Iterator<Double> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            sum += iterator.next();
+            count++;
+        }
+        return sum / count;
+    }
+
+    /**
+     * Gets the standart deviation
+     * @return
+     */
+    public static double getStandartDeviation() {
+        LinkedList<Double> linkedList = createLinkedListFromFile("src/resources/values.txt");
+        double mean = getMean();
+        double sumOfSquaredDifferences = 0;
+        int count = 0;
+        Iterator<Double> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            double value = iterator.next();
+            sumOfSquaredDifferences += Math.pow(value - mean, 2);
+            count++;
+        }
+        double variance = sumOfSquaredDifferences / (count - 1);
+        return Math.sqrt(variance);
+    }
+
+    /**
+     * Creates a linkedList for de Numbers of the files
+     * @param fileName
+     * @return
+     */
+    public static LinkedList<Double> createLinkedListFromFile(String fileName) {
+        LinkedList<Double> linkedList = new LinkedList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                double value = Double.parseDouble(line);
+                linkedList.addLast(value);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+        return linkedList;
     }
 }
